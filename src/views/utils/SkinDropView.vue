13b089ce2cba9d5a5a3d55054611b2c307cc5739
MoYuan-CN <m56042764@163.com>
2025-06-01 03:20:03 +0800

    feat(skindrop): NameMC Support
    支持了 NameMC 链接加载
    微调 Action 脚本，以支持上传

--- src/views/utils/SkinDropView.vue
+++ src/views/utils/SkinDropView.vue
@@ -6,14 +6,17 @@
     import type { THint } from "@/types/Hint";
     import { uploadFileToAlist } from "@/modules/AlistAPI";
     import { useRouter } from "vue-router";
+    import axios from "axios";
 
     const router = useRouter();
 
     const step = ref(0);
+    const okToPass = ref<boolean>(false);
 
     const data = ref<File | null>(null);
+    const dataUrl = ref<string | null>(null);
     const fileInput = ref<HTMLInputElement | null>(null);
-    // const namemcInput = ref<HTMLInputElement | null>(null);
+    const namemcInput = ref<HTMLInputElement | null>(null);
     const fileType = ref<string>("");
 
     const skinview = ref<HTMLCanvasElement | null>(null);
@@ -30,40 +33,39 @@
         const target = event.target as HTMLInputElement;
         const file = target.files?.[0];
         data.value = file!;
-        // namemcInput.value!.disabled = Boolean(file);
+        okToPass.value = true;
+        namemcInput.value!.disabled = Boolean(file);
     };
-    // const handleNamemcInputChange = (event: Event) => {
-    //     const target = event.target as HTMLInputElement;
-    //     const value = target.value;
-    //     if (/https:\/\/namemc\.com\/skin\/[0-9a-f]+/.test(value)) {
-    //         data.value = value;
-    //     } else {
-    //         data.value = null;
-    //     }
-    //     fileInput.value!.disabled = Boolean(value);
-    // };
+    const handleNamemcInputChange = (event: Event) => {
+        const target = event.target as HTMLInputElement;
+        const value = target.value;
+        if (/^https:\/\/(zh\.)?namemc\.com\/skin\/[0-9a-f]+$/.test(value)) {
+            dataUrl.value = value;
+            okToPass.value = true;
+        } else {
+            data.value = null;
+        }
+        fileInput.value!.disabled = Boolean(value);
+    };
 
     // Step 2
     const handleStartPreview = async () => {
         step.value = 1;
-        nextTick(() => {
-            // let skinFile;
-            // switch (typeof data.value) {
-            //     case "string":
-            //         // 从 NameMC 链接获取
-            //         const seprated = data.value.split("/");
-            //         console.log(seprated);
-            //         const url = `https://s.namemc.com/i/${seprated[seprated.length - 1]}.png`;
-            //         // 获取文件
-            //         const response = await axios.get<ArrayBuffer>(url, { responseType: "arraybuffer" });
-            //         skinFile = URL.createObjectURL(new Blob([response.data], { type: response.headers["content-type"] }));
-            //         break;
-            //     default:
-            //         // 从文件获取
-            //         skinFile = URL.createObjectURL(data.value as File);
-            //         break;
-            // }
-            const skinFile = URL.createObjectURL(data.value as File);
+        nextTick(async () => {
+            let skinFile;
+            if (dataUrl.value !== null) {
+                // 从 NameMC 链接获取
+                const seprated = dataUrl.value.split("/");
+                const url = `https://mc521.rth2.xyz/index.php?url=https://s.namemc.com/i/${seprated[seprated.length - 1]}.png`;
+                // 获取文件
+                const response = await axios.get<ArrayBuffer>(url, { responseType: "arraybuffer" });
+                // 保存为 File 对象
+                data.value = new File([response.data], `${seprated[seprated.length - 1]}.png`, { type: "image/png" });
+                skinFile = URL.createObjectURL(new Blob([response.data], { type: response.headers["content-type"] }));
+            } else {
+                // 从文件获取
+                skinFile = URL.createObjectURL(data.value as File);
+            }
             if (!skinViewer.value) {
                 skinViewer.value = new skinview3d.SkinViewer({
                     canvas: skinview.value!,
@@ -75,6 +77,7 @@
             skinViewer.value.animation = new skinview3d.WalkingAnimation();
         });
     };
+
     // Step 3
     const handleUpload = async () => {
         if (!data.value || !username.value) {
@@ -105,7 +108,7 @@
         const { Alist_Url, Alist_Download_Url_Base } = import.meta.env;
         fileType.value = data.value!.type.split("/")[1];
         try {
-            await navigator.clipboard.writeText(`/skin set ${username} ${Alist_Url}/d/${Alist_Download_Url_Base}/${username}.${fileType.value}`);
+            await navigator.clipboard.writeText(`/skin url ${Alist_Url}/d/${Alist_Download_Url_Base}/${username}.${fileType.value}?t=${new Date().getTime()}`);
             EventBus.emit<THint>("hint:create", { type: "success", content: "已复制命令到剪贴板" });
         } catch (err) {
             EventBus.emit<THint>("hint:create", {
@@ -143,7 +146,7 @@
 
     // Mount Hook
     onMounted(() => {
-        EventBus.emit<THint>("hint:create", { type: "info", content: "当前落颜工具版本 v0.1.0" });
+        EventBus.emit<THint>("hint:create", { type: "info", content: "当前落颜工具版本为 v0.1.0" });
     });
 
     // Unmount Hook
@@ -167,8 +170,7 @@
                             <svg xmlns="http://www.w3.org/2000/svg" class="size-6" viewBox="0 0 24 24">
                                 <path
                                     fill="currentColor"
-                                    d="M2 12a9 9 0 0 0 9 9c2.39 0 4.68-.94 6.4-2.6l-1.5-1.5A6.7 6.7 0 0 1 11 19c-6.24 0-9.36-7.54-4.95-11.95S18 5.77 18 12h-3l4 4h.1l3.9-4h-3a9 9 0 0 0-18 0"
-                                />
+                                    d="M2 12a9 9 0 0 0 9 9c2.39 0 4.68-.94 6.4-2.6l-1.5-1.5A6.7 6.7 0 0 1 11 19c-6.24 0-9.36-7.54-4.95-11.95S18 5.77 18 12h-3l4 4h.1l3.9-4h-3a9 9 0 0 0-18 0" />
                             </svg>
                         </button>
                     </Transition>
@@ -191,7 +193,7 @@
                             </div>
                         </Transition>
                         <Transition name="fade" mode="out-in">
-                            <div class="flex flex-col items-center gap-4 absolute" v-if="step === 1 || step === 2">
+                            <div class="w-full h-full flex flex-col justify-center items-center gap-4 absolute" v-if="step === 1 || step === 2">
                                 <canvas ref="skinview" class="bg-base-100 size-128" />
                             </div>
                         </Transition>
@@ -201,54 +203,44 @@
                             <div class="w-full flex flex-col justify-between items-center gap-4 absolute" v-if="step === 0">
                                 <h1 class="text-3xl font-bold">上传你的皮肤</h1>
                                 <fieldset class="fieldset w-1/2">
-                                    <legend class="fieldset-legend">你的皮肤文件，需要是支持的大小</legend>
-                                    <input type="file" class="file-input w-full" ref="fileInput" @input="handleFileInputChange" />
-                                    <!-- <p class="label">也可直接拖拽皮肤文件进入此窗口（DnD）</p> -->
+                                    <legend class="fieldset-legend">你的皮肤文件</legend>
+                                    <input
+                                        type="file"
+                                        class="file-input w-full transition-all duration-200 ease-in-out"
+                                        ref="fileInput"
+                                        @input="handleFileInputChange" />
+                                    <p class="label">需要是支持的大小，例如 32x32 或 64x32 等</p>
                                 </fieldset>
                                 <div class="divider w-9/10 mx-auto"></div>
-                                <!-- TODO: Impl Backend for NameMC -->
-                                <!-- <h1 class="text-2xl font-bold">从 NameMC 链接获取皮肤</h1>
+                                <h1 class="text-2xl font-bold">从 NameMC 链接获取皮肤</h1>
                                 <fieldset class="fieldset w-1/2">
                                     <legend class="fieldset-legend">NameMC 皮肤页面链接</legend>
                                     <label class="input validator w-full">
-                                        <svg
-                                            class="h-[1em] opacity-50"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 24 24">
-                                            <g
-                                                stroke-linejoin="round"
-                                                stroke-linecap="round"
-                                                stroke-width="2.5"
-                                                fill="none"
-                                                stroke="currentColor">
-                                                <path
-                                                    d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path>
-                                                <path
-                                                    d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>
+                                        <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                            <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2.5" fill="none" stroke="currentColor">
+                                                <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path>
+                                                <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>
                                             </g>
                                         </svg>
                                         <input
                                             type="url"
                                             required
-                                            disabled
-                                            placeholder="https://namemc.com/skin/..."
-                                            pattern="^https:\/\/namemc\.com\/skin\/[0-9a-f]+$"
+                                            placeholder="https://namemc.com/skin/... 或 https://zh.namemc.c..."
+                                            pattern="^https:\/\/(zh\.)?namemc\.com\/skin\/[0-9a-f]+$"
                                             title="需要正确的 NameMC 皮肤 URL"
                                             ref="namemcInput"
+                                            class="transition-all duration-200 ease-in-out"
                                             @input="handleNamemcInputChange" />
                                     </label>
-                                    <p class="validator-hint hidden">需要正确的 NameMC 皮肤 URL</p>
+                                    <p class="validator-hint hidden -mt-0.5">需要正确的 NameMC 皮肤 URL</p>
                                     <p class="label">
-                                        * 暂不支持
-                                        <span
-                                            class="underline cursor-help tooltip tooltip-right"
-                                            data-tip="由于 NameMC 服务器限制，无法直接从本工具箱获取到 NameMC 皮肤。此功能将在下一次大更新中推出，敬请期待。">
-                                            为什么？
-                                        </span>
+                                        <a class="underline" href="https://namemc.com" target="_blank">打开 NameMC</a>
+                                        <a class="underline" href="https://zh.namemc.com" target="_blank">打开 NameMC 中国站</a>
+                                        <span class="underline cursor-help tooltip tooltip-right" data-tip="在 NameMC 中打开你喜欢的皮肤，复制网页链接粘贴至上方输入框并确认即可。">如何使用？</span>
                                     </p>
                                 </fieldset>
-                                <div class="divider w-9/10 mx-auto"></div> -->
-                                <button class="btn btn-primary" :disabled="!data" @click="handleStartPreview">确认</button>
+                                <div class="divider w-9/10 mx-auto"></div>
+                                <button class="btn btn-primary" :disabled="!okToPass" @click="handleStartPreview">确认</button>
                             </div>
                         </Transition>
                         <Transition name="fade" mode="out-in">
@@ -273,8 +265,7 @@
                                                 pattern="^[A-Za-z][A-Za-z0-9\-]*$"
                                                 minlength="3"
                                                 maxlength="30"
-                                                title="你的用户名"
-                                            />
+                                                title="你的用户名" />
                                         </label>
                                         <button class="join-item btn btn-success" :disabled="isFinalBtnDisabled" @click="handleUpload">是，我要落颜</button>
                                     </div>
@@ -287,8 +278,7 @@
                                 <div
                                     v-if="uploadStatus"
                                     class="alert mb-4"
-                                    :class="uploadError ? 'alert-error!' : uploadStatus === '正在上传...' ? 'alert-warning' : 'alert-success!'"
-                                >
+                                    :class="uploadError ? 'alert-error!' : uploadStatus === '正在上传...' ? 'alert-warning' : 'alert-success!'">
                                     <!-- Icon -->
                                     <span class="loading loading-dots loading-sm" v-if="!uploadError && uploadStatus === '正在上传...'"></span>
                                     <svg
@@ -296,22 +286,19 @@
                                         xmlns="http://www.w3.org/2000/svg"
                                         class="h-6 w-6 shrink-0 stroke-current"
                                         fill="none"
-                                        viewBox="0 0 24 24"
-                                    >
+                                        viewBox="0 0 24 24">
                                         <path
                                             stroke-linecap="round"
                                             stroke-linejoin="round"
                                             stroke-width="2"
-                                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                                        />
+                                            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                                     </svg>
                                     <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
                                         <path
                                             stroke-linecap="round"
                                             stroke-linejoin="round"
                                             stroke-width="2"
-                                            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-                                        />
+                                            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
                                     </svg>
                                     <!-- Text -->
                                     <span v-if="uploadStatus === '正在上传...'">
